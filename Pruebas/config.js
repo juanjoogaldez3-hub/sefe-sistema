59485a618c9de9e9357a55b66254d20179f15826
Juanjo <juanjoogaldez3@gmail.com>
2026-06-26 22:38:22 -0600

    Corregir config de pruebas

--- Pruebas/config.js
+++ Pruebas/config.js
@@ -2,6 +2,6 @@
 // SEFE — Configuración (config.js)
 // ============================================================
 const SEFE_CONFIG = {
-  url: "https://imvoyzxdvtoktckazzsv.supabase.co/rest/v1/",
+  url: "https://imvoyzxdvtoktckazzsv.supabase.co",
   key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltdm95enhkdnRva3Rja2F6enN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjAwMjYsImV4cCI6MjA5ODA5NjAyNn0.grVM8a132r_UmRDs-6DYc9oPN1nKz_mtHa9LKGdAq4Y"
 };
